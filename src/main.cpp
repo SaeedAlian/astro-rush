23b12c7915945ca2fde28a42f8dafc450eac13c1
SaeedAlian <aliansaeed82@gmail.com>
2026-07-21 11:49:11 +0330

    update: use Mesh & Shader classes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
+#include "mesh.hpp"
+#include "shader.hpp"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
-
-std::string readFile(const char *path) {
-  std::ifstream file(path);
-  std::stringstream buf;
-  buf << file.rdbuf();
-  return buf.str();
-}
 
 int main() {
   if (!glfwInit()) {
@@ -26,7 +19,8 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(1920, 1080, "Endless Runner 3D", nullptr, nullptr);
+  GLFWwindow *window = glfwCreateWindow(
+      1920, 1080, "Endless Runner 3D", nullptr, nullptr);
 
   if (!window) {
     std::cerr << "Failed to create window\n";
@@ -41,81 +35,20 @@ int main() {
     return -1;
   }
 
-  // clang-format off
-  float vertices[] = {
-      // x     y     z   
-      -5.0f, 0.0f, 50.0f, 
-      5.0f, 0.0f, 50.0f, 
-      5.0f, 0.0f, -50.0f, 
-      -5.0f, 0.0f, -50.0f,
-  };
+  Mesh plane = Mesh::createPlane(10.0f, 50.0f);
 
-  unsigned int indices[] = {
-      0, 1, 2, 
-      2, 3, 0,
-  };
-  // clang-format on
-
-  unsigned int VAO;
-  unsigned int VBO;
-  unsigned int EBO;
-
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
-  glEnableVertexAttribArray(0);
-
-  std::string vertexCode = readFile("shaders/vertex.glsl");
-
-  std::string fragmentCode = readFile("shaders/fragment.glsl");
-
-  const char *vertexSource = vertexCode.c_str();
-
-  const char *fragmentSource = fragmentCode.c_str();
-
-  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-  glShaderSource(vertexShader, 1, &vertexSource, nullptr);
-
-  glCompileShader(vertexShader);
-
-  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-  glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
-
-  glCompileShader(fragmentShader);
-
-  unsigned int shaderProgram = glCreateProgram();
-
-  glAttachShader(shaderProgram, vertexShader);
-
-  glAttachShader(shaderProgram, fragmentShader);
-
-  glLinkProgram(shaderProgram);
-
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  Shader shader =
+      Shader("shaders/vertex.glsl", "shaders/fragment.glsl");
 
   glm::vec3 cameraPosition(0.0f, 10.0f, 10.0f);
 
   glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);
 
-  glm::mat4 view = glm::lookAt(cameraPosition, cameraTarget, glm::vec3(0, 1, 0));
+  glm::mat4 view =
+      glm::lookAt(cameraPosition, cameraTarget, glm::vec3(0, 1, 0));
 
-  glm::mat4 projection = glm::perspective(glm::radians(90.0f), 800.0f / 600.0f, 0.1f, 200.0f);
+  glm::mat4 projection = glm::perspective(
+      glm::radians(90.0f), 800.0f / 600.0f, 0.1f, 200.0f);
 
   glm::mat4 model = glm::mat4(1.0f);
 
@@ -130,18 +63,12 @@ int main() {
 
     glEnable(GL_DEPTH_TEST);
 
-    glUseProgram(shaderProgram);
+    shader.use();
+    shader.setMat4("model", model);
+    shader.setMat4("view", view);
+    shader.setMat4("projection", projection);
 
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, &model[0][0]);
-
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, &view[0][0]);
-
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE,
-                       &projection[0][0]);
-
-    glBindVertexArray(VAO);
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    plane.draw();
 
     glfwSwapBuffers(window);
 
