72ea1bfb1d982986005fe82bedb54e067a68603f
SaeedAlian <aliansaeed82@gmail.com>
2026-07-27 22:16:16 +0330

    feat: get automatic screen size in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include "game.hpp"
 
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    return -1;
+  }
+
+  GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+  const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+
   GameOptions opts;
   opts.title = "Astro Rush";
-  Game g = Game(opts);
+  opts.width = mode->width;
+  opts.height = mode->height;
+
+  glfwTerminate();
+
+  Game g(opts);
   g.run();
+
   return 0;
 }
